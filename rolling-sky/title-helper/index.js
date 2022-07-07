357772767f6c1e407a44cac70166862f3e865e70
sqdldev <105597032+sqdldev@users.noreply.github.com>
2022-07-07 17:34:51 -0500

    Add files via upload

---
+++ rolling-sky/title-helper/index.js
@@ -0,0 +1,19 @@
+function generate() {
+	var target = document.getElementById('target');
+	var ttext = target.value.split('\n');
+	var alltxt = [];
+
+	for (let i = 0; i < ttext.length; i++) {
+	const byteArray = [parseInt(ttext[i].length+2)];
+
+	const bytesString = String.fromCharCode(...byteArray);
+
+	var a = bytesString+ttext[i];
+	var b = a.replace('\r','{CR}').replace('\n','{LF}');
+
+	alltxt.push(b);
+	}
+
+	var a = alltxt.toString().replaceAll(',','\n');
+	target.value = a;
+}
